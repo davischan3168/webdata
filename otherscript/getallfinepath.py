47cd3f402cb6316a5d975c6c2c7d9168adbb5bf5
davischan3168 <racheal123@163.com>
2016-08-29 21:41:03 +0800

    update getallfinepath

--- otherscript/getallfinepath.py
+++ otherscript/getallfinepath.py
@@ -1,4 +1,4 @@
-# encoding:utf-8
+y# -*- encoding:utf-8 -*-
 ##
 # 实现批量修改文件夹下说有文件的名称，循环所有的文件夹
 # 
@@ -16,10 +16,13 @@ def write_file(path,r):
 
 for (pf,dirs,files) in os.walk(pf):
     for old in files:
-        #new=tem.replace(']',')')
-        #os.rename(pf+'/'+old,pf+'/'+new)
-        filename=os.path.splitext(old)[0]
-        line= '- [ ]  [[./'+pf+'/'+old+']['+filename+']]\n'
-        write_file('wa.txt',line)
-        #print (old,new)
-        #"""
+        try:
+            #new=tem.replace(']',')')
+            #os.rename(pf+'/'+old,pf+'/'+new)
+            filename=os.path.splitext(old)[0]
+            line= '- [ ]  [[./'+pf+'/'+old+']['+filename+']]\n'
+            write_file('wa.txt',line)
+            #print (old,new)
+            #"""
+        except Exception as e:
+            print (e)
